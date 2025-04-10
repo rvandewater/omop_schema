50d06d37cd7fc077c9bb14291d068ba1b464bfce
Robin van de Water <r.p.vandewater@gmail.com>
2025-04-10 11:11:51 +0200

    sphinx

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -6,37 +6,37 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'omop_schema'
-copyright = '2025, Robin P. van de Water'
-author = 'Robin P. van de Water'
-release = '0.0.4'
+project = "omop_schema"
+copyright = "2025, Robin P. van de Water"
+author = "Robin P. van de Water"
+release = "0.0.4"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src'))  # Adjust the path to your source code
+
+sys.path.insert(0, os.path.abspath(".."))  # Adjust the path to your source code
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'autoapi.extension',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "autoapi.extension",
 ]
 
-autoapi_type = 'python'
-autoapi_dirs = ['../src']  # Path to your source code
+autoapi_type = "python"
+autoapi_dirs = ["../../src/omop_schema/"]  # Adjusted path to your source code
 # templates_path = ['_templates']
 # html_theme = 'sphinx_rtd_theme'
